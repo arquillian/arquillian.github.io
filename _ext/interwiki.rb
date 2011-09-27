08cbbf4b0c7da504d89886c0ee064228b1c1c2f6
Dan Allen <dan.j.allen@gmail.com>
2011-09-27 11:18:30 -0400

    interwiki extension, proper use of grid, content
    - add interwiki extension for shorthand pattern links (e.g., "ARQ-100":issue://ARQ-100
    - apply the blueprint grid correctly on home & features page, using semantic classes
    - additional content for getting started guide
    - additional nobles

---
+++ _ext/interwiki.rb
@@ -0,0 +1,21 @@
+require 'hpricot'
+
+module Awestruct
+  module Extensions
+    module Interwiki
+      def interwiki_urls(text)
+        doc = Hpricot(text)
+        doc.search('//a').each do |a|
+          a['href'] = interwiki_url(a['href'])
+        end
+        doc
+      end
+    
+      def interwiki_url(url)
+        return url.gsub(/^profile:\/\/(.*)/, 'http://community.jboss.org/people/\1') if (url =~ /^profile:\/\//)
+        return url.gsub(/^issue:\/\/(.*)/, 'https://issues.jboss.org/browse/\1') if (url =~ /^issue:\/\//)
+        url
+      end
+    end
+  end
+end
--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,14 +1,19 @@
 require 'fork_me_ribbon'
+require 'interwiki'
 #require 'sassy-buttons'
 
 Awestruct::Extensions::Pipeline.new do
+    # Posts, Paginator, Atomizer and IntenseDebate are the blog extensions
     extension Awestruct::Extensions::Posts.new('/blog')
     extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page=>5)
     extension Awestruct::Extensions::Atomizer.new(:posts, '/blog.atom')
-    extension Awestruct::Extensions::Indexifier.new
     #extension Awestruct::Extensions::IntenseDebate.new()
+
+    # Indexifier moves HTML files to their own directory to achieve "pretty" URLs (e.g., features.html -> /features/index.html)
+    extension Awestruct::Extensions::Indexifier.new
 
     helper Awestruct::Extensions::Partial
     #helper Awestruct::Extensions::GoogleAnalytics
     helper Awestruct::Extensions::ForkMeRibbon
+    helper Awestruct::Extensions::Interwiki
 end
