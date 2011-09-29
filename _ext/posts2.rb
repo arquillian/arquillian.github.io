db7b05c896ceca6f3c1704f8d4ceaba0955f2687
Aslak Knutsen <aslak@redhat.com>
2011-09-29 14:42:28 +0200

    Add new version of Posts Extension, Posts2
    For release GitHub:Release posts, we only need to match with existing template based on "repo-name"-"tag-name".xx, the posts date is fetched from the Tag commit.
    
    The Posts2 Extension support adding Posts in given subfolder to the year/month/day structure based on page.date and not only date parsed from page-name.
    
    In Posts2:
    
    2011-01-01-name.xx
    
    is handled the same as
    
    name.xx
      ---
      date: 2011-01-01
      ---

---
+++ _ext/posts2.rb
@@ -0,0 +1,67 @@
+
+module Awestruct
+  module Extensions
+    class Posts2
+
+      def initialize(path_prefix='', assign_to=:posts)
+        @path_prefix = path_prefix
+        @assign_to   = assign_to
+      end
+
+      def execute(site)
+        posts = []
+ 	
+        site.pages.each do |page|
+          year, month, day, slug = nil
+          
+          if ( page.relative_source_path =~ /^#{@path_prefix}\// )
+            if ( page.relative_source_path =~ /^#{@path_prefix}\/(20[01][0-9])-([01][0-9])-([0123][0-9])-([^.]+)\..*$/ )
+              year  = $1
+              month = $2
+              day   = $3
+              slug  = $4
+              page.date = Time.utc( year.to_i, month.to_i, day.to_i )
+            elsif (page.date)
+              page.relative_source_path =~ /^#{@path_prefix}\/(.*)\..*$/
+              date = page.date;
+              if date.kind_of? String
+                date = Time.parse page.date
+              end
+              year = date.year
+              month = date.month
+              day = date.day
+              page.date = Time.utc(year, month, day)
+              slug = $1
+            end
+
+            # if a date was found create a post
+            if( year and month and day)
+              page.slug = slug
+              context = OpenStruct.new({
+                :site=>site,
+                :page=>page,
+              })
+              #page.body = page.render( context )
+              page.output_path = "#{@path_prefix}/#{year}/#{month}/#{day}/#{slug}.html"
+              #page.layout = 'post'
+              posts << page
+            end
+          end
+          
+        end
+        posts = posts.sort_by{|each| [each.date, each.sequence || 0, File.mtime( each.source_path ), each.slug ] }.reverse
+        
+        last = nil
+        singular = @assign_to.to_s.singularize
+        posts.each do |e|
+          if ( last != nil )
+             e.send( "next_#{singular}=", last )
+             last.send( "previous_#{singular}=", e )
+          end
+          last = e
+        end
+        site.send( "#{@assign_to}=", posts )
+      end
+    end
+  end
+end
