11ba226d5558f556a0e5481eeeb6ab1f6fa16144
Dan Allen <dan.j.allen@gmail.com>
2011-10-08 04:22:01 -0700

    fix blockquote

--- _ext/fork_me_ribbon.rb
+++ _ext/fork_me_ribbon.rb
@@ -5,7 +5,7 @@ module Awestruct
       # Possible to make color configurable in initializer?
       def fork_me_ribbon()
         html = ''
-        html += %Q(<a class="forkme" href="http://github.com/#{site.source_repository}">)
+        html += %Q(<a class="forkme" href="#{site.source_repo}">)
         html += %Q(<img style="position: absolute; top: 0; right: 0; border: 0;" src="/images/forkme_green_skewed.png" alt="Fork me on github"/>)
         html += %Q(</a>)
       end
