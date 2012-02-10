076e294fcea2d94d023026f86ca0090c3d122cd9
Dan Allen <dan.j.allen@gmail.com>
2012-02-10 16:45:58 -0500

    regenerate script

---
+++ _bin/regenerate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BIN_DIR=$(dirname "$0")
+ROOT_DIR="$BIN_DIR/.."
+SITE_DIR="$ROOT_DIR/_site"
+SASS_CACHE_DIR="$ROOT_DIR/.sass-cache"
+
+rm -rf "$SITE_DIR"
+rm -rf "$SASS_CACHE_DIR"
+
+awestruct -s
