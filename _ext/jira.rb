8ce5b3650543ccad1ac705f8d68217cbc46c0e43
Dan Allen <dan.j.allen@gmail.com>
2012-11-05 16:03:11 -0700

    fix escaped quotes in issue text

--- _ext/jira.rb
+++ _ext/jira.rb
@@ -34,7 +34,7 @@ module Awestruct::Extensions::Jira
 
         url = @base_url + RELEASE_NOTES_PATH_TEMPLATE % [@project_id, v['id']]
         html = RestClient.get url, :cache_key => "jira/release-notes-#{@project_key}-#{v['id']}.html"
-        doc = Nokogiri::HTML(html.gsub(/<(\/)?textarea/, '<\\1div'))
+        doc = Nokogiri::HTML(html.gsub(/<(\/)?textarea/, '<\\1div').gsub(/&amp;(#[0-9]+|[a-z]+);/, '&\\1;'))
         release_notes = OpenStruct.new({
           :id => v['id'],
           :comment => v['description'],
