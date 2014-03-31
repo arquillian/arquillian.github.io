43135f2b6c76e0f53d210ead66204203419822b9
Aslak Knutsen <aslak@4fs.no>
2014-03-31 18:40:33 +0200

    Published master to GitHub pages.

--- javascripts/jquery-jgfeed.js
+++ javascripts/jquery-jgfeed.js
@@ -13,4 +13,4 @@
  *   http://me.boo.uz
  *
  */
-!function($){$.extend({jGFeed:function(url,fnk,num,key){if(null==url)return!1;var gurl="http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q="+url;null!=num&&(gurl+="&num="+num),null!=key&&(gurl+="&key="+key),$.getJSON(gurl,function(data){return"function"!=typeof fnk?!1:(fnk.call(this,data.responseData.feed),void 0)})}})}(jQuery);
+!function($){$.extend({jGFeed:function(url,fnk,num,key){if(null==url)return!1;var gurl="http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q="+url;null!=num&&(gurl+="&num="+num),null!=key&&(gurl+="&key="+key),$.getJSON(gurl,function(data){return"function"!=typeof fnk?!1:void fnk.call(this,data.responseData.feed)})}})}(jQuery);
