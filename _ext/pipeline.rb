c339bc9eb080ac7453ae28993c820050c1920d84
Aslak Knutsen <aslak@4fs.no>
2015-11-24 17:17:05 +0000

    Move Arquillian Liferay to GitHub Issues

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -58,6 +58,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Github::ReleaseNotes.new('arquillian/arquillian-container-chameleon', 'chameleon')
   extension Awestruct::Extensions::Github::ReleaseNotes.new('arquillian/arquillian-governor', 'governor')
   extension Awestruct::Extensions::Github::ReleaseNotes.new('arquillian/arquillian-universe-bom', 'bom')
+  extension Awestruct::Extensions::Github::ReleaseNotes.new('arquillian/arquillian-extension-liferay', 'liferay')
   extension Awestruct::Extensions::Repository::Collector.new(480465, 'd4a1e23d6ac12e6a11767d2a5521e2698e3ebd0e0262acbed3331b4ff79ffe0a', :observers => [github_collector])
   extension Awestruct::Extensions::Identities::Collect.new(github_collector)
   extension Awestruct::Extensions::Identities::Crawl.new(
