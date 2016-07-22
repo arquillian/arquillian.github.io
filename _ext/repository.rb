31b8424f93174be5bc27cc639cbff43836c1297e
Bartosz Majsak <bartosz.majsak@gmail.com>
2016-07-22 21:38:01 +0200

    Fixes modules pages generation. previous code was resulting with 404.

--- _ext/repository.rb
+++ _ext/repository.rb
@@ -328,22 +328,24 @@ module Awestruct
 
         def generate_pages(site)
           site.modules.each_pair {|t, modules|
-            modules.each { |m|
+            modules.each {|m|
               module_page_basepath = m.basepath + '-' + t.dasherize
               if !site.engine.nil?
                 module_page = site.engine.load_site_page('modules/_module.html.haml')
               else
                 module_page = OpenStruct.new
               end
-              module_page.output_path = "modules/#{module_page_basepath}.html"
-              # FIXME why is this needed here?
+              module_page.output_path = "modules/#{module_page_basepath}/index.html"
               module_page.url = "/modules/#{module_page_basepath}"
               module_page.module = m
               module_page.component = m.component
               module_page.link_name = m.name.sub(/^Arquillian /, '')
               module_page.title = m.name
               m.page = module_page
+              site.engine.set_urls([module_page])
+              site.engine.generate_page(module_page) # TODO why is it needed all of the sudden?
               site.pages << module_page
+
             }
           }
         end
