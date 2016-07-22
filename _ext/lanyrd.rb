9e53f408c4e7d68ad717030a8f4ad784901fd978
Bartosz Majsak <bartosz.majsak@gmail.com>
2016-07-22 21:35:19 +0200

    Displays future talks from lanyrd even when there is no concrete date yet (only conference)

--- _ext/lanyrd.rb
+++ _ext/lanyrd.rb
@@ -4,6 +4,7 @@ require 'tzinfo'
 require 'nokogiri'
 require 'rest-client'
 require 'parallel'
+require 'uri'
 require_relative 'common.rb'
 ##
 # Lanyrd is an Awestruct extension module for interacting with lanyrd.com
@@ -51,61 +52,67 @@ module Awestruct
     def initialize(term)
       @base = 'http://lanyrd.com'
       @term = term
-      @since = Time.now
+      @now = Time.now
     end
   
     def execute(site)
+      site.sessions = site.sessions || []
       @lanyrd_tmp = tmp(site.tmp_dir, 'lanyrd')
       
       # add &topic=#{@term} to limit sessions to those that have been tagged with the term
-      # context=future only works for conferences atm...waiting for session search support
-      search_url = "#{@base}/search/?type=session&q=#{@term}"
-      
-      sessions = Hamster::Set.new
-      
-      pages = Hamster::Set.new
-      begin 
-        page1 = Nokogiri::HTML(getOrCache(File.join(@lanyrd_tmp, "search-#{@term}-1.html"), search_url))
-      rescue => e
-        # In case lanyrd is down... which is not that rare
-        puts e
-        site.sessions = []
-        return
-      end
-      pages = pages << page1
-      
-      extract_pages(page1, pages, search_url)
-      Parallel.each(pages, progress: "Extracting sessions details from Lanyrd" ) do |page|
-        extract_sessions(page, sessions)
-      end
-      sessions = sessions.sort_by{|session| session.start_datetime}
-      site.sessions = sessions.size.times.map { |i| session.at(i) }
+      site.sessions = Parallel.map([@now.year, (@now.year + 1)], progress: "Fetching Arquillian sessions from Lanyrd") { |year|
+        params = {:type => 'session', :year => year}
+        search_url = search_sessions_url @term, params
+        begin
+          page1 = Nokogiri::HTML(getOrCache(File.join(@lanyrd_tmp, cache_file_name(@term, params)), search_url))
+        rescue => e
+          # In case lanyrd is down... which is not that rare
+          puts e
+          return []
+        end
+
+        extract_pages(page1, params).unshift(extract_sessions(page1)).flatten
+      }.flatten.select { |s|
+        (not s.start_datetime.nil? and s.start_datetime >= @now) or (not s.conference_start_datetime.nil? and s.conference_start_datetime >= @now)
+      }
     end
-    
+
+    def cache_file_name(term, params, index = 1)
+      urlParams = params.map { |k,v| "#{k}-#{v}"}.join('-')
+      "search-#{term}-#{urlParams}-#{index}.html"
+    end
+
     # Find all Pages in a 'root' Page
-    def extract_pages(root, pages, search_url)
+    def extract_pages(root, params)
       # lanyrd pageination show 1, 2, 3... 5
       # Get the last index and loop
       last_page_index = 1
       root.css('div[@class*=pagination]').each do |p|
-        last_page_index = Integer(p.search('li').last.at('a').inner_text) +1
+        last_page_index = Integer(p.search('li').last.at('a').inner_text) + 1
       end
-      Parallel.each(2...last_page_index, progress: "Fetching Arquillian sessions from Lanyrd") { |index|
-        pageinated_url = "#{search_url}&page=#{index}"
-        pageX = Nokogiri::HTML(getOrCache(File.join(@lanyrd_tmp, "search-#{@term}-#{index}.html"), pageinated_url))
-        pages << pageX
+      search_url =  search_sessions_url @term, params
+      Parallel.map(2...last_page_index) { |index|
+        paginated_url = "#{search_url}&page=#{index}"
+        pageX = Nokogiri::HTML(getOrCache(File.join(@lanyrd_tmp, cache_file_name(@term, params, index)), paginated_url))
+        puts extract_sessions(pageX)
+        return 1
       }
     end
-    
+
+    def search_sessions_url(query, params = {})
+      urlParams = URI.escape(params.map { |k,v| "#{k}=#{v}"}.join('&')).insert 0, '&'
+      "#{@base}/search/?type=session&q=#{query}#{urlParams}"
+    end
+
     # Find all sessions in Page
-    def extract_sessions(page, sessions)
-      page.css('li[@class*=s-session]').each do |raw|
-        
+    def extract_sessions(page)
+      page.css('li[@class*=s-session]').map do |raw|
+
         event_link = raw.css('h3').at('a')
-        
+
         session = OpenStruct.new
         session.title = event_link.inner_text
-        
+
         session_detail_url = "#{@base}#{event_link.attribute('href')}"
         session.slug = event_link.attribute('href').to_s.split('/').last
         session_detail = Nokogiri::HTML(getOrCache(File.join(@lanyrd_tmp, "session-#{session.slug}.html"), session_detail_url))
@@ -117,70 +124,64 @@ module Awestruct
           session.timezone = timezone_node.attribute('title').to_s
         end
 
-        tz = nil
-        tz = TZInfo::Timezone.get(session.timezone.to_s) unless session.timezone.nil?
+        session.start_datetime = convert_to_date(session_meta_node.css('abbr[@class=dtstart]').first, session.timezone)
+        session.end_datetime = convert_to_date(session_meta_node.at('abbr[@class=dtend]'), session.timezone )
 
-        dtstart_node = session_meta_node.css('abbr[@class=dtstart]').first
-        if dtstart_node
-          session.start_datetime = Time.parse dtstart_node.attribute('title')
-          session.start_datetime = tz.local_to_utc(session.start_datetime) unless tz.nil?
+        session.detail_url = session_detail_url
+        conference_startdate_node = session_detail.css('div[@class*=conference]').first.css('p[@class=date]').css('abbr[@class=dtstart]').first
+        session.conference_start_datetime = convert_to_date(conference_startdate_node, session.timezone )
+        session.event_location = session_detail.css('p[@class=location]').first.inner_text.split(/\//).map{|l| l.strip}.reverse.join(', ')
+
+        session.speaker_names = []
+        session.speakers = []
+
+        raw.css('p[@class*=meta]').each do |meta|
+          type = meta.css('strong').inner_html
+          meta.css('strong').remove()
+          session.event = meta.inner_text.strip if type.eql? 'Event'
+          session.event_url = "#{meta.css('a').first.attribute('href')}" if type.eql? 'Event'
+
+          if type.eql? 'Speakers'
+            session.speaker_names = meta.inner_text.strip.split(', ')
+            meta.css('a').each do |speaker_node|
+              name = speaker_node.inner_text.strip
+              username = speaker_node.attribute('href').match(/\/profile\/([^\/]*)/)[1]
+              # QUESTION add identities for speakers if not exist?
+              session.speakers << {'name' => name, 'username' => username }
+            end
+          end
         end
 
-        # skip old events and events with no date
-        if session.start_datetime and session.start_datetime >= @since
-          dtend_node = session_meta_node.at('abbr[@class=dtend]')
-          if dtend_node
-            session.end_datetime = Time.parse dtend_node.attribute('title')
-            session.end_datetime = tz.local_to_utc(session.end_datetime) unless tz.nil?
+        # updated Speaker lanyrd layout 2013-06-26
+        session_detail.css('ul[@class*=user-list]').each do |users|
+          name = nil
+          users.css('span[@class*=user-name]').each do |user|
+            name = user.text()
+            session.speaker_names << name
           end
-
-          session.description = session_detail.css('div[@class*=abstract]').first.inner_html
-          session.detail_url = session_detail_url
-
-          session.event_location = session_detail.css('p[@class=location]').first.inner_text.split(/\//).map{|l| l.strip}.reverse.join(', ')
-          
-          session.speaker_names = []
-          session.speakers = []
-
-          raw.css('p[@class*=meta]').each do |meta|
-            type = meta.css('strong').inner_html
-            meta.css('strong').remove()
-            session.event = meta.inner_text.strip if type.eql? 'Event'
-            session.event_url = "#{meta.css('a').first.attribute('href')}" if type.eql? 'Event'
-            
-            if type.eql? 'Speakers'
-              session.speaker_names = meta.inner_text.strip.split(', ')
-              meta.css('a').each do |speaker_node|
-                name = speaker_node.inner_text.strip
-                username = speaker_node.attribute('href').match(/\/profile\/([^\/]*)/)[1]
-                # QUESTION add identities for speakers if not exist?
-                session.speakers << {'name' => name, 'username' => username }
-                end
-            end
+          username = nil
+          users.css('li a').each do |a|
+            username = a.attribute('href').to_s.match(/\/profile\/([^\/]*)/)[1]
           end
-
-          # updated Speaker lanyrd layout 2013-06-26
-          session_detail.css('ul[@class*=user-list]').each do |users|
-            name = nil
-            users.css('span[@class*=user-name]').each do |user|
-              name = user.text()
-              session.speaker_names << name
-            end
-            username = nil
-            users.css('li a').each do |a|
-              username = a.attribute('href').to_s.match(/\/profile\/([^\/]*)/)[1]
-            end
-            session.speakers << {'name' => name, 'username' => username }
-          end
-          
-          sessions = sessions << session
+          session.speakers << {'name' => name, 'username' => username }
         end
+        session
       end
+    end
+
+    def convert_to_date(date_node, timezone)
+      tz = TZInfo::Timezone.get(timezone.to_s) unless timezone.nil?
+      datetime = nil
+      if date_node
+        datetime = Time.parse date_node.attribute('title')
+        datetime = tz.local_to_utc(datetime) unless tz.nil?
+      end
+      return datetime
     end
   end
 
   ##
-  # Awestruct::Extensions::Lanyrd::Export exports sessions retrived by the Search extension
+  # Awestruct::Extensions::Lanyrd::Export exports sessions retrieved by the Search extension
   # as a ical stream.
   #
   # This class is loaded as an extension in the Awestruct pipeline. The
@@ -204,9 +205,9 @@ module Awestruct
       if site.sessions
 
         cal = RiCal.Calendar do |cal|
-          site.sessions.each do |session|
-
+          site.sessions.select { |s| not s.start_datetime.nil? }.each do |session|
             cal.event do |e|
+              puts e
               e.dtstart = session.start_datetime
               e.dtend = session.end_datetime
               e.summary = session.title
