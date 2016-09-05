d30e1573679b6716c21d68a90dcf2e88623495b6
Bartosz Majsak <bartosz.majsak@gmail.com>
2016-09-05 09:51:45 +0200

    Makes cache restore script more versatile
    Creates _tmp/lanyrd folder if does not exists yet before
    copying the content

--- _backup/restore_cache.sh
+++ _backup/restore_cache.sh
@@ -3,11 +3,14 @@
 LIGHT_GREEN='\033[1;32m'
 GREEN='\033[0;32m'
 CLEAR='\033[0m'
+CACHE_DIR=../_tmp/lanyrd 
 
 echo -e "${LIGHT_GREEN}-> Updating timestamp of all cached files ...${CLEAR}"
 find . -exec touch {} \;
 
 echo -e "${LIGHT_GREEN}-> Restoring to _tmp ...${CLEAR}"
-cp -R * ../_tmp
+[ -d $CACHE_DIR ] || mkdir -p $CACHE_DIR
+rm -rf $CACHE_DIR/*
+cp -R lanyrd/* $CACHE_DIR
 
 echo -e "${GREEN}Done${CLEAR}"
