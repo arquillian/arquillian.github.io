2d4aac8e4b8bde6de9d14934e953ba31b6f65997
Matous Jobanek <mjobanek@redhat.com>
2017-06-19 16:47:54 +0200

    fix: fixed restore_cache script to make it runnable from any location

--- _backup/restore_cache.sh
+++ _backup/restore_cache.sh
@@ -3,7 +3,8 @@
 LIGHT_GREEN='\033[1;32m'
 GREEN='\033[0;32m'
 CLEAR='\033[0m'
-CACHE_DIR=../_tmp/lanyrd 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CACHE_DIR=${SCRIPT_DIR}/../_tmp/lanyrd
 
 echo -e "${LIGHT_GREEN}-> Updating timestamp of all cached files ...${CLEAR}"
 find . -exec touch {} \;
@@ -11,6 +12,6 @@ find . -exec touch {} \;
 echo -e "${LIGHT_GREEN}-> Restoring to _tmp ...${CLEAR}"
 [ -d $CACHE_DIR ] || mkdir -p $CACHE_DIR
 rm -rf $CACHE_DIR/*
-cp -R lanyrd/* $CACHE_DIR
+cp -R ${SCRIPT_DIR}/lanyrd/* $CACHE_DIR
 
 echo -e "${GREEN}Done${CLEAR}"
