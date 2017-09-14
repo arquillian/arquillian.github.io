e21f579f4852db478d50b9fcbc37f9dbaa056991
Matous Jobanek <mjobanek@redhat.com>
2017-09-14 17:48:20 +0200

    fixed release notes and description

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -69,7 +69,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Github::ReleaseNotes.new('arquillian/arquillian-universe-bom', 'bom')
   extension Awestruct::Extensions::Github::ReleaseNotes.new('arquillian/arquillian-extension-liferay', 'liferay')
   extension Awestruct::Extensions::Github::ReleaseNotes.new('arquillian/arquillian-container-wls', 'wls')
-  extension Awestruct::Extensions::Github::ReleaseNotes.new('arquillian/smart-testing', 'smart-testing')
+  extension Awestruct::Extensions::Github::ReleaseNotes.new('arquillian/smart-testing', 'testing')
 
   extension Awestruct::Extensions::Repository::Collector.new(:observers => [github_collector])
 
