a0a9d6913297ae368ec27dfd86239e702091d3dd
Scott M Stark <starksm@starkinternational.com>
2025-08-20 22:00:25 -0600

    First attempt at converting to Roq
    Signed-off-by: Scott M Stark <starksm@starkinternational.com>

--- _bin/contributors.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-CONTRIBUTORS=`grep -h 'login' _tmp/github/contributors-* | sed 's/ *"login": "\([^"]\+\)",\?/\1/' | sort | uniq`
-for c in $CONTRIBUTORS; do
-  echo $c
-done
-echo $CONTRIBUTORS | wc -w
